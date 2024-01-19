aa7a866a00b6ec6dd8b0ea68c2bfdd9ebd86825b
darshita <darshita.golaviya@arcticgrey.com>
2024-01-19 18:54:47 +0530

    feat: update url

--- src/helpers/api_helper.js
+++ src/helpers/api_helper.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-axios.defaults.baseURL = "http://demo-api.tarifftales.com";
+axios.defaults.baseURL = "https://demo-api.tarifftales.com";
 // axios.defaults.baseURL = "http://localhost:7005";
 
 // axios.defaults.headers.common["Authorization"] = `${token}`
