947e2e330cc8badeed2760e57cf0fc9045ffc4f9
darshita <darshita.golaviya@arcticgrey.com>
2024-03-20 19:07:00 +0530

    fix: customer api

--- src/store/Parties/Customer/saga.js
+++ src/store/Parties/Customer/saga.js
@@ -1,6 +1,6 @@
 import { all, call, fork, put, takeEvery, takeLatest } from "redux-saga/effects";
 import { showErrorToast, showSuccessToast } from "../../../components/Common/CustomToast";
-import { getCustomerDataByIdSer, getCustomerDataSer, postCustomerCompanySer, postCustomerContactSer, postCustomerDocSer } from "../../../helpers/services/PartiesService";
+import { getCustomerDataByIdSer, getCustomerDataSer, getCustomerListSer, postCustomerCompanySer, postCustomerContactSer, postCustomerDocSer } from "../../../helpers/services/PartiesService";
 import { GET_CUSTOMERS_ID, GET_CUSTOMER_BY_ID, GET_CUSTOMER_BY_ID_SUCCESS, GET_CUSTOMER_LOADER, GET_PARTIES_CUSTOMER_DETAILS_TYPE, GET_PARTIES_CUSTOMER_DETAILS_TYPE_SUCCESS, UPLOAD_CUSTOMER_COMPANYDATA_TYPE, UPLOAD_CUSTOMER_CONTACT_TYPE, UPLOAD_CUSTOMER_DOCUMENT_TYPE } from "./actiontype";
 import axios from "axios";
 import { Get_File_URL } from "../../../helpers/url_helper";
@@ -8,22 +8,39 @@ import { Get_File_URL } from "../../../helpers/url_helper";
 function* fetchPartiesCustomerSaga({ payload}) {
     yield put({type: GET_CUSTOMER_LOADER, payload: true});
     try {
-        const response = yield call(getCustomerDataSer, payload);
-        
-        if (response && response.content && response.content) {
-            response?.content?.forEach(element => {
-                let imageData = element.logoPath;
-                const base64Encoded = window.btoa(imageData);
-                element.logo =(!!(imageData)? `${axios.defaults.baseURL}${Get_File_URL}${base64Encoded}`:'');
-                element.documents?.forEach(doc=>{
-                    let imageData = doc.documentPath;
+        if(payload){
+            const response = yield call(getCustomerDataSer, payload);   
+            if (response && response.content && response.content) {
+                response?.content?.forEach(element => {
+                    let imageData = element.logoPath;
                     const base64Encoded = window.btoa(imageData);
-                    doc.logo =(!!(imageData)? `${axios.defaults.baseURL}${Get_File_URL}${base64Encoded}`:'');
-                })
-            });
+                    element.logo =(!!(imageData)? `${axios.defaults.baseURL}${Get_File_URL}${base64Encoded}`:'');
+                    element.documents?.forEach(doc=>{
+                        let imageData = doc.documentPath;
+                        const base64Encoded = window.btoa(imageData);
+                        doc.logo =(!!(imageData)? `${axios.defaults.baseURL}${Get_File_URL}${base64Encoded}`:'');
+                    })
+                });
+            }
+            yield put({ type: GET_PARTIES_CUSTOMER_DETAILS_TYPE_SUCCESS, payload: response });
+            yield put({type: GET_CUSTOMER_LOADER, payload: false});                     
+        } else {
+            const response = yield call(getCustomerListSer);
+            if (response && response.content && response.content) {
+                response?.content?.forEach(element => {
+                    let imageData = element.logoPath;
+                    const base64Encoded = window.btoa(imageData);
+                    element.logo =(!!(imageData)? `${axios.defaults.baseURL}${Get_File_URL}${base64Encoded}`:'');
+                    element.documents?.forEach(doc=>{
+                        let imageData = doc.documentPath;
+                        const base64Encoded = window.btoa(imageData);
+                        doc.logo =(!!(imageData)? `${axios.defaults.baseURL}${Get_File_URL}${base64Encoded}`:'');
+                    })
+                });
+            }
+            yield put({ type: GET_PARTIES_CUSTOMER_DETAILS_TYPE_SUCCESS, payload: response });
+            yield put({type: GET_CUSTOMER_LOADER, payload: false});
         }
-        yield put({ type: GET_PARTIES_CUSTOMER_DETAILS_TYPE_SUCCESS, payload: response });
-        yield put({type: GET_CUSTOMER_LOADER, payload: false});
     } catch (error) {
         console.log(error, "saga customer api error");
         yield put({type: GET_CUSTOMER_LOADER, payload: false});
